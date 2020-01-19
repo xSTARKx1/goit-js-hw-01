f5e8f128a94408a388a5ef4b655061ddb1fcb2ce
Viktor <victor.k9560@gmail.com>
2020-01-19 14:39:42 +0200

    rework

--- js/task_04.js
+++ js/task_04.js
@@ -2,12 +2,14 @@ const credits = 23580;
 const pricePerDroid = 3000;
 
 const numberOfDroids = prompt('Введите количество дроидов!');
-const priceOfOrder = pricePerDroid * numberOfDroids;
-const remainder = credits - priceOfOrder;
+const totalPrice = pricePerDroid * numberOfDroids;
+const remainder = credits - totalPrice;
 
 if (numberOfDroids === null) {
   console.log('Отменено пользователем!');
-} else if (priceOfOrder > credits) {
+}
+
+if (totalPrice > credits) {
   console.log('Недостаточно средств на счету!');
 } else {
   console.log(
